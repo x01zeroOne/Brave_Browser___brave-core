michael-spengler/brave-core

Rewrite most `Foo* field_` pointer fields to `raw_ptr<Foo> field_`.
Follow the lead of upstream and rewrite most raw pointers to using
raw_ptr<T> instead, making also sure that they are initialized.

This rewrite affected all *.h and *.cc files in this repository
except those from the ios/, third_party/ and vendor/ directories,
and it didn't touch any *.mm file either.

Regular expression used for the search & replace process:

   Find regexp: "^\s*?(\S*?)\* (\S*?[a-zA-Z0-9]_?);"
  Replace with: "raw_ptr<$1> $2 = nullptr;"

Chromium change:

https://source.chromium.org/chromium/chromium/src/+/0e45c020c43b1a9f6d2870ff7f92b30a2f03a458

commit 0e45c020c43b1a9f6d2870ff7f92b30a2f03a458
Author: Keishi Hattori <keishi@chromium.org>
Date:   Sat Nov 27 09:25:52 2021 +0000

    Rewrite most `Foo* field_` pointer fields to `raw_ptr<Foo> field_`.

    DO NOT REVERT (unless absolutely necessary)! Report build breaks to keishi@(APAC)/glazunov@(EMEA /sebmarchand@(NA) as soon as you see them. Fixes are expected to be trivial.

    This commit was generated automatically, by running the following script: tools/clang/rewrite_ra _ptr_fields/rewrite-multiple-platforms.sh on commit fe74bc434e5b7e92d13a328362fcb6df15d8847e

    For more information, see MiraclePtr One Pager [1], the PSA at chromium-dev@ [2], and the raw_pt  documentation in //base/memory/raw_ptr.md.

    FYI This CL does not enable MiraclePtr protection and we expect no behavior change from this.

    [1] https://docs.google.com/document/d/1pnnOAIz_DMWDI4oIOFoMAqLnf_MZ2GsrJNb_dbQ3ZBg/edit?usp=sha ing
    [2] https://groups.google.com/a/chromium.org/g/chromium-dev/c/vAEeVifyf78/m/SkBUc6PhBAAJ

    Bug: 1272324, 1073933

browser/brave_wallet/brave_wallet_service_unittest.cc
@@ -5,6 +5,7 @@
 
 #include "brave/components/brave_wallet/browser/brave_wallet_service.h"
 
+#include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -436,7 +437,7 @@ class BraveWalletServiceUnitTest : public testing::Test {
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<base::HistogramTester> histogram_tester_;
   std::unique_ptr<BraveWalletService> service_;
-  KeyringController* keyring_controller_;
+  raw_ptr<KeyringController> keyring_controller_ = nullptr;
   std::unique_ptr<TestBraveWalletServiceObserver> observer_;
   base::test::ScopedFeatureList scoped_feature_list_;
 

components/brave_wallet/browser/eth_pending_tx_tracker.h
@@ -10,6 +10,7 @@
 
 #include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
@@ -50,9 +51,9 @@ class EthPendingTxTracker {
   // (txHash, count)
   base::flat_map<std::string, uint8_t> dropped_blocks_counter_;
 
-  EthTxStateManager* tx_state_manager_;
-  EthJsonRpcController* rpc_controller_;
-  EthNonceTracker* nonce_tracker_;
+  raw_ptr<EthTxStateManager> tx_state_manager_ = nullptr;
+  raw_ptr<EthJsonRpcController> rpc_controller_ = nullptr;
+  raw_ptr<EthNonceTracker> nonce_tracker_ = nullptr;
 
   base::WeakPtrFactory<EthPendingTxTracker> weak_factory_;
 };

x01zeroOne/Brave_Browser___brave-core

browser/brave_wallet/brave_wallet_service_unittest.cc
@@ -5,6 +5,7 @@
 
 #include "brave/components/brave_wallet/browser/brave_wallet_service.h"
 
+#include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -499,7 +500,7 @@ class BraveWalletServiceUnitTest : public testing::Test {
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<base::HistogramTester> histogram_tester_;
   std::unique_ptr<BraveWalletService> service_;
-  KeyringService* keyring_service_;
+  raw_ptr<KeyringService> keyring_service_ = nullptr;
   JsonRpcService* json_rpc_service_;
   EthTxService* eth_tx_service_;
   std::unique_ptr<TestBraveWalletServiceObserver> observer_;

components/brave_wallet/browser/eth_pending_tx_tracker.h
@@ -10,6 +10,7 @@
 
 #include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
@@ -59,9 +60,9 @@ class EthPendingTxTracker {
   // (txHash, count)
   base::flat_map<std::string, uint8_t> dropped_blocks_counter_;
 
-  EthTxStateManager* tx_state_manager_;
-  JsonRpcService* json_rpc_service_;
-  EthNonceTracker* nonce_tracker_;
+  raw_ptr<EthTxStateManager> tx_state_manager_ = nullptr;
+  raw_ptr<JsonRpcService> json_rpc_service_ = nullptr;
+  raw_ptr<EthNonceTracker> nonce_tracker_ = nullptr;
 
   base::WeakPtrFactory<EthPendingTxTracker> weak_factory_;
 };