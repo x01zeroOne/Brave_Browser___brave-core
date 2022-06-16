kim0/brave-core

TranslateBubbleView instantiated by TranslateBubbleController
chromium change:
https://chromium.googlesource.com/chromium/src/+/cdc0d04be76fbb9fe1a39844605cbced61687704

commit cdc0d04be76fbb9fe1a39844605cbced61687704
Author: Anthony Cui <cuianthony@google.com>
Date:   Thu May 19 21:16:53 2022 +0000

    Integrate TranslateBubbleController for full page translate and tests

    Bug: 1313957

chromium_src/chrome/browser/ui/views/translate/translate_bubble_controller.cc
@@ -1,0 +1,27 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/translate/translate_bubble_controller.h"
+#include "brave/browser/ui/views/translate/brave_translate_bubble_view.h"
+#include "brave/components/translate/core/common/brave_translate_features.h"
+#include "brave/components/translate/core/common/buildflags.h"
+
+// static
+template <typename... Args>
+std::unique<TranslateBubbleView>
+TranslateBubbleController::MakeTranslateBubbleView(Args&&... args) {
+#if BUILDFLAG(ENABLE_BRAVE_TRANSLATE_EXTENSION)
+  if (translate::IsTranslateExtensionAvailable()) {
+    return std::make_unique<BraveTranslateBubbleView>(
+        std::forward<Args>(args)...);
+  }
+#endif  // BUILDFLAG(ENABLE_BRAVE_TRANSLATE_EXTENSION)
+  return new std::make_unique<TranslateBubbleView>(std::forward<Args>(args)...);
+}
+
+#define MAKE_BRAVE_TRANSLATE_BUBBLE_VIEW MakeTranslateBubbleView
+
+#include "src/chrome/browser/ui/views/translate/translate_bubble_controller.h"
+#undef MAKE_BRAVE_TRANSLATE_BUBBLE_VIEW

x01zeroOne/Brave_Browser___brave-core

chromium_src/chrome/browser/ui/views/translate/translate_bubble_controller.cc
@@ -1,0 +1,27 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/translate/translate_bubble_controller.h"
+#include "brave/browser/ui/views/translate/brave_translate_bubble_view.h"
+#include "brave/components/translate/core/common/brave_translate_features.h"
+#include "brave/components/translate/core/common/buildflags.h"
+
+// static
+template <typename... Args>
+std::unique_ptr<TranslateBubbleView>
+TranslateBubbleController::MakeTranslateBubbleView(Args&&... args) {
+#if BUILDFLAG(ENABLE_BRAVE_TRANSLATE_EXTENSION)
+  if (translate::IsTranslateExtensionAvailable()) {
+    return std::make_unique<BraveTranslateBubbleView>(
+        std::forward<Args>(args)...);
+  }
+#endif  // BUILDFLAG(ENABLE_BRAVE_TRANSLATE_EXTENSION)
+  return std::make_unique<TranslateBubbleView>(std::forward<Args>(args)...);
+}
+
+#define MAKE_BRAVE_TRANSLATE_BUBBLE_VIEW MakeTranslateBubbleView
+
+#include "src/chrome/browser/ui/views/translate/translate_bubble_controller.cc"
+#undef MAKE_BRAVE_TRANSLATE_BUBBLE_VIEW