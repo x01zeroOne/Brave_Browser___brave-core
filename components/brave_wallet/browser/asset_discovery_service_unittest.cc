3153cde359c3071f4deb542c5c8cb3b616450499
Nick von Pentz <nickvonpentz@protonmail.com>
2022-08-26 13:28:35 -0400

    Remove import and update copyright year for asset_discovery_service_unittest.cc

--- components/brave_wallet/browser/asset_discovery_service_unittest.cc
+++ components/brave_wallet/browser/asset_discovery_service_unittest.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -9,7 +9,6 @@
 #include <utility>
 
 #include "base/test/bind.h"
-#include "brave/browser/brave_wallet/brave_wallet_service_factory.h"
 #include "brave/browser/brave_wallet/json_rpc_service_factory.h"
 #include "brave/browser/brave_wallet/keyring_service_factory.h"
 #include "brave/browser/brave_wallet/tx_service_factory.h"
