abbaslanbay/brave-core

Added blank wireguard service

components/brave_vpn/browser/connection/win/brave_vpn_wireguard_service/brave_wireguard_manager.h
@@ -1,0 +1,41 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_
+
+#include <wrl/implements.h>
+#include <wrl/module.h>
+
+#include <windows.h>
+
+#include "base/win/windows_types.h"
+#include "brave/components/brave_vpn/browser/connection/win/brave_vpn_wireguard_service/brave_wireguard_manager_idl.h"
+
+namespace brave_vpn {
+
+class BraveWireguardManager
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IBraveWireguardManager> {
+ public:
+  BraveWireguardManager() = default;
+
+  BraveWireguardManager(const BraveWireguardManager&) = delete;
+  BraveWireguardManager& operator=(const BraveWireguardManager&) = delete;
+
+  IFACEMETHODIMP EnableVpn(const wchar_t* config, DWORD* last_error) override;
+  IFACEMETHODIMP DisableVpn(DWORD* last_error) override;
+  IFACEMETHODIMP GenerateKeypair(BSTR* public_key,
+                                 BSTR* private_key,
+                                 DWORD* last_error) override;
+
+ private:
+  ~BraveWireguardManager() override = default;
+};
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_

components/brave_vpn/browser/connection/win/brave_vpn_wireguard_service/scoped_sc_handle.h
@@ -1,0 +1,42 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_
+
+#include "base/types/expected.h"
+#include "base/win/scoped_handle.h"
+
+// Helper for methods which perform system operations which may fail. The
+// failure reason is returned as an HRESULT.
+// TODO(crbug.com/1369769): Remove the following warning once resolved in
+// base. NOTE: When ValueT is an integral type, base::expected's implicit ctors
+// are ambiguous. To return an error in this case it must be wrapped in a
+// base::unexpected(error);
+template <typename ValueT>
+using HResultOr = base::expected<ValueT, HRESULT>;
+
+class ScHandleTraits {
+ public:
+  using Handle = SC_HANDLE;
+
+  ScHandleTraits() = delete;
+  ScHandleTraits(const ScHandleTraits&) = delete;
+  ScHandleTraits& operator=(const ScHandleTraits&) = delete;
+
+  static bool CloseHandle(SC_HANDLE handle) {
+    return ::CloseServiceHandle(handle) != FALSE;
+  }
+
+  static bool IsHandleValid(SC_HANDLE handle) { return handle != nullptr; }
+
+  static SC_HANDLE NullHandle() { return nullptr; }
+};
+
+using ScopedScHandle =
+    base::win::GenericScopedHandle<ScHandleTraits,
+                                   base::win::DummyVerifierTraits>;
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_

components/brave_vpn/browser/connection/win/brave_vpn_wireguard_service/service_constants.h
@@ -1,0 +1,27 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_
+
+#include <guiddef.h>
+#include <string>
+
+namespace brave_vpn {
+
+constexpr wchar_t kBraveWireguardServiceExecutable[] =
+    L"brave_vpn_wireguard_service.exe";
+constexpr wchar_t kBraveVpnServiceRegistryStoragePath[] =
+    L"Software\\BraveSoftware\\Brave\\Vpn\\BraveWireguardService";
+constexpr char kBraveWgServiceInstall[] = "install";
+
+const CLSID& GetBraveWireguardServiceClsid();
+const IID& GetBraveWireguardServiceIid();
+std::wstring GetBraveWireguardServiceName();
+std::wstring GetBraveWireguardServiceDisplayName();
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_

components/brave_vpn/browser/connection/win/brave_vpn_wireguard_service/service_main.h
@@ -1,0 +1,78 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_
+
+#include <windows.h>
+
+#include "base/functional/callback.h"
+#include "base/no_destructor.h"
+
+namespace brave_vpn {
+
+class ServiceMain {
+ public:
+  static ServiceMain* GetInstance();
+
+  ServiceMain(const ServiceMain&) = delete;
+  ServiceMain& operator=(const ServiceMain&) = delete;
+
+  // Start() is the entry point called by WinMain.
+  int Start();
+
+  // Creates an out-of-proc WRL Module.
+  void CreateWRLModule();
+
+  // Registers the Service COM class factory object so other applications can
+  // connect to it. Returns the registration status.
+  HRESULT RegisterClassObject();
+
+  // Unregisters the Service COM class factory object.
+  void UnregisterClassObject();
+  void SignalExit();
+
+ private:
+  friend class base::NoDestructor<ServiceMain>;
+
+  ServiceMain();
+  ~ServiceMain();
+
+  // This function handshakes with the service control manager and starts
+  // the service.
+  int RunAsService();
+
+  // Runs the service on the service thread.
+  void ServiceMainImpl();
+
+  // The control handler of the service.
+  static void WINAPI ServiceControlHandler(DWORD control);
+
+  // The main service entry point.
+  static void WINAPI ServiceMainEntry(DWORD argc, wchar_t* argv[]);
+
+  // Calls ::SetServiceStatus().
+  void SetServiceStatus(DWORD state);
+
+  // Handles object registration, message loop, and unregistration. Returns
+  // when all registered objects are released.
+  HRESULT Run();
+  // Calls ::CoInitializeSecurity to allow all users to create COM objects
+  // within the server.
+  static HRESULT InitializeComSecurity();
+
+  // The action routine to be executed.
+  int (ServiceMain::*run_routine_)();
+
+  // Identifier of registered class objects used for unregistration.
+  DWORD cookies_[1];
+  SERVICE_STATUS_HANDLE service_status_handle_;
+  SERVICE_STATUS service_status_;
+  base::OnceClosure quit_;
+};
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_

x01zeroOne/Brave_Browser___brave-core

components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/brave_wireguard_manager.h
@@ -1,0 +1,41 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_
+
+#include <wrl/implements.h>
+#include <wrl/module.h>
+
+#include <windows.h>
+
+#include "base/win/windows_types.h"
+#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/brave_wireguard_manager_idl.h"
+
+namespace brave_vpn {
+
+class BraveWireguardManager
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IBraveWireguardManager> {
+ public:
+  BraveWireguardManager() = default;
+
+  BraveWireguardManager(const BraveWireguardManager&) = delete;
+  BraveWireguardManager& operator=(const BraveWireguardManager&) = delete;
+
+  IFACEMETHODIMP EnableVpn(const wchar_t* config, DWORD* last_error) override;
+  IFACEMETHODIMP DisableVpn(DWORD* last_error) override;
+  IFACEMETHODIMP GenerateKeypair(BSTR* public_key,
+                                 BSTR* private_key,
+                                 DWORD* last_error) override;
+
+ private:
+  ~BraveWireguardManager() override = default;
+};
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_BRAVE_WIREGUARD_MANAGER_H_

components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/scoped_sc_handle.h
@@ -1,0 +1,42 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_
+
+#include "base/types/expected.h"
+#include "base/win/scoped_handle.h"
+
+// Helper for methods which perform system operations which may fail. The
+// failure reason is returned as an HRESULT.
+// TODO(crbug.com/1369769): Remove the following warning once resolved in
+// base. NOTE: When ValueT is an integral type, base::expected's implicit ctors
+// are ambiguous. To return an error in this case it must be wrapped in a
+// base::unexpected(error);
+template <typename ValueT>
+using HResultOr = base::expected<ValueT, HRESULT>;
+
+class ScHandleTraits {
+ public:
+  using Handle = SC_HANDLE;
+
+  ScHandleTraits() = delete;
+  ScHandleTraits(const ScHandleTraits&) = delete;
+  ScHandleTraits& operator=(const ScHandleTraits&) = delete;
+
+  static bool CloseHandle(SC_HANDLE handle) {
+    return ::CloseServiceHandle(handle) != FALSE;
+  }
+
+  static bool IsHandleValid(SC_HANDLE handle) { return handle != nullptr; }
+
+  static SC_HANDLE NullHandle() { return nullptr; }
+};
+
+using ScopedScHandle =
+    base::win::GenericScopedHandle<ScHandleTraits,
+                                   base::win::DummyVerifierTraits>;
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SCOPED_SC_HANDLE_H_

components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/service_constants.h
@@ -1,0 +1,27 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_
+
+#include <guiddef.h>
+#include <string>
+
+namespace brave_vpn {
+
+constexpr wchar_t kBraveWireguardServiceExecutable[] =
+    L"brave_vpn_wireguard_service.exe";
+constexpr wchar_t kBraveVpnServiceRegistryStoragePath[] =
+    L"Software\\BraveSoftware\\Brave\\Vpn\\BraveWireguardService";
+constexpr char kBraveWgServiceInstall[] = "install";
+
+const CLSID& GetBraveWireguardServiceClsid();
+const IID& GetBraveWireguardServiceIid();
+std::wstring GetBraveWireguardServiceName();
+std::wstring GetBraveWireguardServiceDisplayName();
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_CONSTANTS_H_

components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/service_main.h
@@ -1,0 +1,78 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_
+#define BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_
+
+#include <windows.h>
+
+#include "base/functional/callback.h"
+#include "base/no_destructor.h"
+
+namespace brave_vpn {
+
+class ServiceMain {
+ public:
+  static ServiceMain* GetInstance();
+
+  ServiceMain(const ServiceMain&) = delete;
+  ServiceMain& operator=(const ServiceMain&) = delete;
+
+  // Start() is the entry point called by WinMain.
+  int Start();
+
+  // Creates an out-of-proc WRL Module.
+  void CreateWRLModule();
+
+  // Registers the Service COM class factory object so other applications can
+  // connect to it. Returns the registration status.
+  HRESULT RegisterClassObject();
+
+  // Unregisters the Service COM class factory object.
+  void UnregisterClassObject();
+  void SignalExit();
+
+ private:
+  friend class base::NoDestructor<ServiceMain>;
+
+  ServiceMain();
+  ~ServiceMain();
+
+  // This function handshakes with the service control manager and starts
+  // the service.
+  int RunAsService();
+
+  // Runs the service on the service thread.
+  void ServiceMainImpl();
+
+  // The control handler of the service.
+  static void WINAPI ServiceControlHandler(DWORD control);
+
+  // The main service entry point.
+  static void WINAPI ServiceMainEntry(DWORD argc, wchar_t* argv[]);
+
+  // Calls ::SetServiceStatus().
+  void SetServiceStatus(DWORD state);
+
+  // Handles object registration, message loop, and unregistration. Returns
+  // when all registered objects are released.
+  HRESULT Run();
+  // Calls ::CoInitializeSecurity to allow all users to create COM objects
+  // within the server.
+  static HRESULT InitializeComSecurity();
+
+  // The action routine to be executed.
+  int (ServiceMain::*run_routine_)();
+
+  // Identifier of registered class objects used for unregistration.
+  DWORD cookies_[1];
+  SERVICE_STATUS_HANDLE service_status_handle_;
+  SERVICE_STATUS service_status_;
+  base::OnceClosure quit_;
+};
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_COMPONENTS_BRAVE_VPN_BROWSER_CONNECTION_WIREGUARD_WIN_BRAVE_VPN_WIREGUARD_SERVICE_SERVICE_MAIN_H_