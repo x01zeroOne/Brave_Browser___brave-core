d0a68dec91846a7a8f9bf11a60313e2a96125819
boocmp <boocmp@yandex.ru>
2023-05-05 16:15:06 +0700

    Lint.

--- browser/speedreader/speedreader_tab_helper.cc
+++ browser/speedreader/speedreader_tab_helper.cc
@@ -68,8 +68,9 @@ std::u16string GetSpeedreaderData(
   return result + u"}\n\n";
 }
 
-constexpr const char* kPropertyPrefNames[] = {
-    kSpeedreaderPrefTheme, kSpeedreaderPrefFontSize, kSpeedreaderPrefFontFamily};
+constexpr const char* kPropertyPrefNames[] = {kSpeedreaderPrefTheme,
+                                              kSpeedreaderPrefFontSize,
+                                              kSpeedreaderPrefFontFamily};
 
 SpeedreaderTabHelper::SpeedreaderTabHelper(content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
@@ -301,8 +302,7 @@ void SpeedreaderTabHelper::ShowReaderModeToolbar() {
 
 void SpeedreaderTabHelper::HideReaderModeToolbar() {
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents());
-  static_cast<BraveBrowserWindow*>(browser->window())
-      ->HideReaderModeToolbar();
+  static_cast<BraveBrowserWindow*>(browser->window())->HideReaderModeToolbar();
 }
 
 Profile* SpeedreaderTabHelper::GetProfile() const {
